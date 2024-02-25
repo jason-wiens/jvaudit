d4508024331b36d5facf8fcef1d9d65b73eeed91
Jason Wiens <jwiens@integrity-tech.ca>
2024-02-25 16:02:42 -0700

    init commit

---
+++ src/app/(private)/app/audits/page.tsx
@@ -0,0 +1,3 @@
+export default function Audits() {
+  return <div>Audits</div>;
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,56 @@
 import type { Config } from "tailwindcss";
 
-const config: Config = {
+const config = {
+  darkMode: ["class"],
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
   ],
+  prefix: "",
   theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
+    fontSize: {
+      xs: "0.5rem",
+      sm: "0.75rem",
+      base: "0.875rem",
+      lg: "1rem",
+      xl: "1.125rem",
+      "2xl": "1.25rem",
+      "3xl": "1.5rem",
+      "4xl": "1.875rem",
+      "5xl": "2.25rem",
+      "6xl": "3rem",
+      "7xl": "4rem",
+    },
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        primary: {
+          100: "#025D9C",
+          500: "#01385E",
+          900: "#001A2B",
+        },
+        secondary: {
+          100: "#00BEC4",
+          500: "#00999E",
+          900: "#006063",
+        },
+        accent: {
+          100: "#FF00AC",
+          500: "#E10098",
+          900: "#A5006F",
+        },
       },
     },
   },
-  plugins: [],
-};
+  plugins: [require("tailwindcss-animate")],
+} satisfies Config;
+
 export default config;
