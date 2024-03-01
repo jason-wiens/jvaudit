4d73288f4438fec442c9512499131c4825534333
Jason Wiens <jwiens@integrity-tech.ca>
2024-03-01 08:27:07 -0700

    update .gitignore

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,5 +1,8 @@
 import React from "react";
 
 export default function Login() {
-  return <div>Login</div>;
+  <div className="w-full h-[100vh] flex justify-center items-center">
+    <div className="w-full max-w-sm">{/* <LoginModal /> */}</div>
+    <div className="w-80 h-[800px] bg-secondary-500/20 absolute left-1/2 top-1/2 -z-10 -translate-y-1/2"></div>
+  </div>;
 }
